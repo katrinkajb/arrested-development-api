756695a84ec9ac2f652cd6dc5494a463b9c1f176
kb <katrinkjb@gmail.com>
2021-12-14 11:37:49 -0800

    commenting out test for now

--- setup-db.js
+++ setup-db.js
@@ -2,5 +2,6 @@ const pool = require('./lib/utils/pool.js');
 const setup = require('./data/setup.js');
 
 setup(pool)
+    // eslint-disable-next-line no-console
     .catch((err) => console.error(err))
     .finally(() => process.exit());

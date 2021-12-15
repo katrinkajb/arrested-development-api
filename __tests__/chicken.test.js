6bcab2d0fe97931e66176a086924a17ba5eb8270
Julianne Vela <hello@juliannevela.dev>
2021-12-14 19:11:57 -0800

    updated seed data logic to run npm child process before all tests and that seems to have resolved the random order of test results on each run

--- __tests__/chicken.test.js
+++ __tests__/chicken.test.js
@@ -1,58 +1,58 @@
 require('dotenv').config();
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
-const setup = require('../data/setup');
+const { execSync } = require('child_process');
 const pool = require('../lib/utils/pool');
 
 describe('app routes', () => {
-    beforeEach(() => {
-        return setup(pool);
+  beforeAll(() => {
+    execSync('npm run setup-db');
+  });
+
+  afterAll((done) => {
+    return pool.end(done);
+  });
+  describe('Chicken dance tests', () => {
+    test('returns all chicken dance gifs', async () => {
+      const chickens = [
+        {
+          url: 'https://tv-fanatic-res.cloudinary.com/iu/s--Dx2MU33g--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1407322355/arrested-development-dancing-gif.gif',
+        },
+        {
+          url: 'https://c.tenor.com/Ggx4JAo3QaIAAAAC/arrested-development-chicken.gif',
+        },
+        {
+          url: 'https://64.media.tumblr.com/tumblr_lj6zc4WwBL1qzwuo3.gif',
+        },
+        {
+          url: 'https://media.tumblr.com/tumblr_lj6zd2mwbo1qzwuo3.gif',
+        },
+        {
+          url: 'https://24.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo2_250.gif',
+        },
+        {
+          url: 'https://24.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo3_250.gif',
+        },
+        {
+          url: 'https://25.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo4_250.gif',
+        },
+        {
+          url: 'https://25.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo1_250.gif',
+        },
+      ];
+      const data = await fakeRequest(app)
+        .get('/chicken')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(data.body).toEqual(chickens);
     });
 
-    afterAll((done) => {
-        return pool.end(done);
+    test('returns a random chicken dance gif', async () => {
+      const data = await fakeRequest(app)
+        .get('/chicken/random')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(data.body).toEqual(expect.any(Object));
     });
-    describe.skip('Chicken dance tests', () => {
-        test('returns all chicken dance gifs', async () => {
-            const chickens = [
-                {
-                    url: 'https://tv-fanatic-res.cloudinary.com/iu/s--Dx2MU33g--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1407322355/arrested-development-dancing-gif.gif',
-                },
-                {
-                    url: 'https://c.tenor.com/Ggx4JAo3QaIAAAAC/arrested-development-chicken.gif',
-                },
-                {
-                    url: 'https://64.media.tumblr.com/tumblr_lj6zc4WwBL1qzwuo3.gif',
-                },
-                {
-                    url: 'https://media.tumblr.com/tumblr_lj6zd2mwbo1qzwuo3.gif',
-                },
-                {
-                    url: 'https://24.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo2_250.gif',
-                },
-                {
-                    url: 'https://24.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo3_250.gif',
-                },
-                {
-                    url: 'https://25.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo4_250.gif',
-                },
-                {
-                    url: 'https://25.media.tumblr.com/tumblr_m2sts9O1BI1r7vuvuo1_250.gif',
-                },
-            ];
-            const data = await fakeRequest(app)
-                .get('/chicken')
-                .expect('Content-Type', /json/)
-                .expect(200);
-            expect(data.body).toEqual(chickens);
-        });
-
-        test('returns a random chicken dance gif', async () => {
-            const data = await fakeRequest(app)
-                .get('/chicken/random')
-                .expect('Content-Type', /json/)
-                .expect(200);
-            expect(data.body).toEqual(expect.any(Object));
-        });
-    });
+  });
 });
--- data/setup.js
+++ data/setup.js
@@ -1,11 +1,7 @@
 const fs = require('fs').promises;
-const { seedTables } = require('./load-seed-data.js');
 
 module.exports = (pool) => {
-    return fs
-        .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
-        .then((sql) => pool.query(sql))
-        .then(() => {
-            seedTables();
-        });
+  return fs
+    .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
+    .then((sql) => pool.query(sql));
 };
